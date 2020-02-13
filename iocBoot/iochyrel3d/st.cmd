11ff57a13d7e44de9bd939b220e8ca8a83b7bf27
K. Gofron <kgofron@comcast.net>
2020-02-13 14:53:11 -0500

    refine startup

--- iocBoot/iochyrel3d/st.cmd
+++ iocBoot/iochyrel3d/st.cmd
@@ -3,7 +3,21 @@
 #- You may have to change hyrel3d to something else
 #- everywhere it appears in this file
 
-#< envPaths
+< envPaths
+
+## Location of stream protocol files
+epicsEnvSet("ENGINEER",                 "kgofron x5283")
+epicsEnvSet("LOCATION",                 "XF11ID-M3")
+epicsEnvSet("STREAM_PROTOCOL_PATH",     "protocol")
+
+epicsEnvSet("SYS",   			"XF:11ID-M3")
+epicsEnvSet("SYSPORT",  		"HYREL")
+epicsEnvSet("CTSYS",			"XF:11ID-CT")
+epicsEnvSet("TSADR",			"192.168.1.51")
+
+epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST", 	"NO")
+epicsEnvSet("EPICS_CA_ADDR_LIST",	"192.168.1.255")
+
 
 ## Register all support components
 dbLoadDatabase("../../dbd/hyrel3d.dbd",0,0)
