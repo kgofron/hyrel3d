58c6b9e83c6ad6ac3b3bee44d070df22b9adf3be
K. Gofron <kgofron@comcast.net>
2020-03-09 16:49:59 -0400

    Refine fan control and databases

--- iocBoot/iochyrel3d/st.cmd
+++ iocBoot/iochyrel3d/st.cmd
@@ -29,12 +29,14 @@ hyrel3d_registerRecordDeviceDriver(pdbbase)
 drvAsynIPPortConfigure("$(SYSPORT)","192.168.1.52:4001")
 
 ## Load record instances
-#dbLoadRecords("../../db/devhyrel3d.db","P=$(Sys),R=$(Dev),PORT=$(SYSPORT),A=0,user=kaz")
+#dbLoadRecords("../../db/devhyrel3d.db","P=$(Sys),R=$(Dev),PORT=$(SYSPORT),A=0")
 dbLoadRecords("../../db/hyrel3d.db")
-# 1 head (T12), N=1
-#dbLoadRecords("../../db/hyrel3d_masterT.db","Sys=$(Sys),Dev=$(Dev),Yoke=1,Head=2,N=1,Heads='T12',PORT=$(SYSPORT),A=0,user=kaz")
-# 2 heads (T11,T12), N=2
-dbLoadRecords("../../db/hyrel3d_masterT.db","Sys=$(Sys),Dev=$(Dev),Yoke=1,Head=1,N=2,Heads='T11,T12',PORT=$(SYSPORT),A=0,user=kaz")
+# 1 head (T12 = Krakatoa), N=1
+#dbLoadRecords("../../db/hyrel3d_masterT.db","Sys=$(Sys),Dev=$(Dev),Yoke=1,Head=2,N=1,Heads='T12',PORT=$(SYSPORT),A=0")
+# 2 heads (T11=MK1,T12=Krakatoa), N=2
+dbLoadRecords("../../db/hyrel3d_masterT.db","Sys=$(Sys),Dev=$(Dev),Yoke=1,Head=1,N=2,Heads='T11,T12',PORT=$(SYSPORT),A=0")
+# Fan control for MK1 is manually configured (previously in hyrel3D_masterT.db)
+dbLoadRecords("../../db/hyrel3d_fan.db","Sys=$(Sys),Dev=$(Dev),Yoke=1,Head=1,PORT=$(SYSPORT),A=0")
 dbLoadRecords("../../db/asynRecord.db")
 
 #var streamDebug 1
